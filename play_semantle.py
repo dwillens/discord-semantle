17aed8f6cd34da8198f8ec0f42bd34290927e9bc
Daniel Willenson <dwillens@hotmail.com>
2022-05-05 19:49:41 -0400

    add shelve-based persistence

--- play_semantle.py
+++ play_semantle.py
@@ -6,137 +6,73 @@ import logging
 import numpy as np
 import random
 import re
+import shelve
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-class PlaySemantle(discord.Client):
-    def __init__(self, *args, **kwargs):
+class GameState:
+    def __init__(self, word, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-        self.filter = re.compile("[^a-zA-Z]")
-
-        with open("words.txt") as f:
-            self.words = [l.strip() for l in f.readlines()]
-
-        self.choose_new_word()
-
-        with open("words_alpha.txt") as f:
-            self.valid = [l.strip() for l in f.readlines()]
-
-    def choose_new_word(self):
-        self.word = self.words[random.randrange(len(self.words))]
+        self.word = word
         self.guesses = dict()
-        self.top = dict()
-        logger.debug(f"{self.word}")
 
-    async def on_ready(self):
-        logger.info(f"We have logged in as {self.user}")
+    def add_word(self, result):
+        self.guesses[self.word] = result
+        self.guesses[self.word]["similarity"] = 100.0
 
-    async def on_message(self, message):
-        if message.author == self.user:
-            pass
+    def add_guess(self, guess, result):
+        self.guesses[guess] = result
 
-        elif not "semantle" in message.channel.name:
-            pass
+        wa = self.guesses[self.word]["array"]
+        ga = self.guesses[guess]["array"]
+        self.guesses[guess]["similarity"] = (
+            100 * np.dot(wa, ga) / (np.linalg.norm(wa) * np.linalg.norm(ga))
+        )
 
-        elif message.content.startswith("!new"):
-            await message.channel.send(self.format_top(20))
-            await message.channel.send(f"old word was {self.word}. choosing a new word")
-            self.choose_new_word()
-
-        elif message.content.startswith("!hint"):
-            gs = [self.guesses[i] for (_, i) in reversed(sorted(self.top.items()))]
-
-            if not gs:
-                n = 1
-            elif not "percentile" in gs[0]:
-                n = 1
-            elif 999 <= gs[0]["percentile"]:
-                n = gs[0]["percentile"] - 1
-                for g in gs[1:]:
-                    if not "percentile" in g:
-                        break
-                    elif n > g["percentile"]:
-                        break
-                    else:
-                        n = g["percentile"] - 1
-            else:
-                n = int((1000 + gs[0]["percentile"]) / 2)
-
-            hint = await self.nth_nearby(n)
-
-            if not self.word in self.guesses:
-                self.guesses[self.word] = await self.result(self.word)
-                self.guesses[self.word]["similarity"] = 100.0
-
-            try:
-                await self.do_guess(message, "hint", hint)
-            except ValueError:
-                await message.channel.send("could not generate hint")
-            except json.decoder.JSONDecodeError:
-                await message.channel.send("could not generate hint")
-
-        elif message.content.startswith("!guess") or message.content.startswith("$"):
-            if not self.word in self.guesses:
-                self.guesses[self.word] = await self.result(self.word)
-                self.guesses[self.word]["similarity"] = 100.0
-
-            strip = 7
-            if message.content.startswith("$"):
-                strip = 1
-
-            guess = self.filter.sub("", message.content[strip:])
-
-            try:
-                await self.do_guess(message, message.author, guess)
-            except ValueError:
-                await message.channel.send(f"{guess} is invalid")
-            except json.decoder.JSONDecodeError:
-                await message.channel.send(f"{guess} is invalid")
-
-        elif message.content.startswith("!top"):
-            n = 10
-            try:
-                n = int(message.content.split(" ")[1])
-            except:
-                pass
-
-            await message.channel.send(self.format_top(n))
-
-    def format_top(self, n):
-        lines = [
-            self.format_guess(guess)
-            for (_, guess) in reversed(sorted(self.top.items())[-n:])
-        ]
-        text = "\n".join(lines)
-        return f"```{text} ```"
-
-    async def do_guess(self, message, author, guess):
-        if not guess in self.valid:
-            raise ValueError("not in list")
-
-        if not guess in self.guesses:
-            self.guesses[guess] = await self.result(guess)
-
-            wa = self.guesses[self.word]["array"]
-            ga = self.guesses[guess]["array"]
-            sim = 100 * np.dot(wa, ga) / (np.linalg.norm(wa) * np.linalg.norm(ga))
-            self.guesses[guess]["similarity"] = sim
-            self.top[sim] = guess
-
+    def maybe_add_author(self, guess, author):
         if not "by" in self.guesses[guess]:
             self.guesses[guess]["by"] = author
 
-        await message.channel.send(f"```{self.format_guess(guess)} ```")
+    def top(self):
+        by_sim = [(v["similarity"], k) for (k, v) in self.guesses.items() if "by" in v]
+        return [k for (_, k) in reversed(sorted(by_sim))]
 
-        if self.word == guess:
-            self.top[100.0] = guess
-            g = self.guesses[guess]
-            await message.channel.send(
-                f':confetti_ball: {g["by"]} got the correct word `{self.word}`'
-            )
+    def hint(self):
+        top = [self.guesses[guess] for guess in self.top()]
+
+        if not top:
+            return 1
+        elif not "percentile" in top[0]:
+            return 1
+        elif 999 <= top[0]["percentile"]:
+            n = top[0]["percentile"] - 1
+            for g in top[1:]:
+                if not "percentile" in g:
+                    break
+                elif n > g["percentile"]:
+                    break
+                else:
+                    n = g["percentile"] - 1
+            return n
+        else:
+            return int((1000 + top[0]["percentile"]) / 2)
+
+    def is_guessed(self, guess):
+        return guess in self.guesses
+
+    def is_win(self, guess):
+        return self.word == guess
+
+    def format_win(self, guess):
+        g = self.guesses[guess]
+        return f':confetti_ball: {g["by"]} got the correct word `{self.word}`'
+
+    def format_top(self, n):
+        lines = [self.format_guess(guess) for guess in self.top()[:n]]
+        text = "\n".join(lines)
+        return f"```{text} ```"
 
     def format_guess(self, guess):
         g = self.guesses[guess]
@@ -149,20 +85,121 @@ class PlaySemantle(discord.Client):
         by = str(g["by"])[:8]
         return f"{guess:15} {percentile:>4} {similarity:6} {by:>8}"
 
-    async def result(self, guess):
+
+class PlaySemantle(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.filter = re.compile("[^a-zA-Z]")
+
+        with open("secretwords.json") as f:
+            self.words = json.loads(f.read())
+
+        self.games = shelve.open("play_semantle")
+
+    async def on_ready(self):
+        logger.info(f"We have logged in as {self.user}")
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            pass
+
+        elif not "semantle" in message.channel.name:
+            pass
+
+        else:
+            if not str(message.channel.id) in self.games:
+                word = self.words[random.randrange(len(self.words))]
+                game = GameState(word)
+
+                result = await self.result(game, word)
+                game.add_word(result)
+
+                self.games[str(message.channel.id)] = game
+                self.games.sync()
+
+                logger.debug(
+                    f"{message.channel}({str(message.channel.id)})'s' word is {word}"
+                )
+
+            if message.content.startswith("!new"):
+                await self.process_new(message)
+
+            elif message.content.startswith("!hint"):
+                await self.process_hint(message)
+
+            elif message.content.startswith("!guess"):
+                guess = self.filter.sub("", message.content[7:])
+                await self.process_guess(message, str(message.author), guess)
+
+            elif message.content.startswith("$"):
+                guess = self.filter.sub("", message.content[1:])
+                await self.process_guess(message, str(message.author), guess)
+
+            elif message.content.startswith("!top"):
+                try:
+                    n = int(message.content.split(" ")[1])
+                except:
+                    n = 10
+
+                await self.process_top(message, n)
+
+    async def process_new(self, message):
+        game = self.games[str(message.channel.id)]
+
+        await message.channel.send(game.format_top(20))
+        await message.channel.send(f"old word was {game.word}. choosing a new word")
+
+        del self.games[str(message.channel.id)]
+        self.games.sync()
+
+    async def process_hint(self, message):
+        game = self.games[str(message.channel.id)]
+        n = game.hint()
+
+        hint = await self.nth_nearby(game, n)
+
+        await self.process_guess(message, "hint", hint)
+
+    async def process_top(self, message, n):
+        game = self.games[str(message.channel.id)]
+        await message.channel.send(game.format_top(n))
+
+    async def process_guess(self, message, author, guess):
+        game = self.games[str(message.channel.id)]
+        try:
+            if not game.is_guessed(guess):
+                result = await self.result(game, guess)
+
+                game.add_guess(guess, result)
+
+            game.maybe_add_author(guess, author)
+            self.games[str(message.channel.id)] = game
+            self.games.sync()
+
+            await message.channel.send(f"```{game.format_guess(guess)} ```")
+
+            if game.is_win(guess):
+                await message.channel.send(game.format_win(guess))
+
+        except json.decoder.JSONDecodeError:
+            await message.channel.send(f"{guess} is invalid")
+
+
+    async def result(self, game, guess):
         async with aiohttp.ClientSession() as session:
             async with session.get(
-                f"http://semantle.novalis.org/model2/{self.word}/{guess}"
+                f"http://semantle.novalis.org/model2/{game.word}/{guess}"
             ) as response:
                 text = await response.text()
                 result = json.loads(text)
                 result["array"] = np.array(result["vec"])
                 return result
 
-    async def nth_nearby(self, n):
+    async def nth_nearby(self, game, n):
         async with aiohttp.ClientSession() as session:
             async with session.get(
-                f"http://semantle.novalis.org/nth_nearby/{self.word}/{n}"
+                f"http://semantle.novalis.org/nth_nearby/{game.word}/{n}"
             ) as response:
                 text = await response.text()
                 result = json.loads(text)
